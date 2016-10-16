b4a9182a3d789c02e1bfede8028febcf48480c55
Elon Lee <Elon@elondemacbook-pro.local>
2016-10-16 21:12:16 +0800

    初始化了BDJ项目

---
+++ ElonExBDJ19/ElonExBDJ19/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElonExBDJ19
+//
+//  Created by Elon Lee on 16/10/16.
+//  Copyright © 2016年 ElonLee Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ElonExBDJ19/ElonExBDJ19/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElonExBDJ19
+//
+//  Created by Elon Lee on 16/10/16.
+//  Copyright © 2016年 ElonLee Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
